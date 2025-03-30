17efc7b05c7fb008fd245a8cee600a54d4deff32
Killian Lopez <killian.lopez@supdevinci-edu.fr>
2025-03-30 15:32:07 +0200

    update

--- docs/js/events/eventHandler.js
+++ docs/js/events/eventHandler.js
@@ -1,4 +1,6 @@
 import { inInConstruction } from "../dataModel/global.js";
+import { updateGauges } from "../update/gauge.js";
+import { updateAll } from "../update/update.js";
 
 document.addEventListener('DOMContentLoaded', () => {
 	const banner = document.getElementById("construction-banner");
